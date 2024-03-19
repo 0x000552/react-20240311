8b19e60bfab15b1620050089252133a58f7ec289
Artyom Afonin <0x000552@gmail.com>
2024-03-20 01:25:14 +0800

    add preferred rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    "semi": [2, "always"],
+    "no-var": 1,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
